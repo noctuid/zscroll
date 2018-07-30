cf2cb8e421d766e22b4cd237a0f2b14969498c9a
Fox Kiester <noct@posteo.net>
2018-07-30 19:50:19 -0400

    Update contact info

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='zscroll',
       version='1.0',
       description='A text scroller for use with panels',
       author='Fox Kiester',
-      author_email='noct[at]openmailbox[dot]org',
+      author_email='noct[at]posteo[dot]net',
       url='https://github.com/noctuid/zscroll',
       license='Simplified BSD',
       # to the bin
